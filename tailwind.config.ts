b55f73de9154cb975f65690bd7ac57c96fa17de4
jayLapani <jaylapani45@gmaill.com>
2024-03-02 21:39:25 +0530

    ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,15 @@ const config = {
     },
     screens:{
       'xs': {'max': '350px'},
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
     },
     extend: {
       colors: {
